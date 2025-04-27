3a707bd0a2e7fcb214c4155c50480dabd2752259
SantiagoBarrientosF <barrientosantiago1090@gmail.com>
2025-04-27 15:34:31 -0500

    Changes : web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DownloadController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('profile');
 });
 
 Route::get('/about_me', function() {
